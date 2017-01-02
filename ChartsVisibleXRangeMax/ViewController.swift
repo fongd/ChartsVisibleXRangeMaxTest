d26392c946d4e3c09d41be5daea8c5b750e98ac8
Derek Fong <fong@subtitled.com>
2017-01-02 01:33:30 -0500

    Initial checkin

--- ChartsVisibleXRangeMax/ViewController.swift
+++ ChartsVisibleXRangeMax/ViewController.swift
@@ -7,12 +7,46 @@
 //
 
 import UIKit
+import Charts
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var chartView: LineChartView!
+    
+    // number of random data points to generate
+    let dataPoints: Int = 10
+    
+    // max number of values we'll show in viewport
+    let maxValuesInViewport: Double = 4.0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+
+        chartView.scaleXEnabled = false
+        chartView.scaleYEnabled = false
+        chartView.pinchZoomEnabled = false
+        chartView.doubleTapToZoomEnabled = false
+
+        var values: [ChartDataEntry] = [ChartDataEntry]()
+        
+        for i in 0 ..< dataPoints {
+            let value = arc4random_uniform(UInt32(500))
+            values.append(ChartDataEntry(x: Double(i), y: Double(value)))
+        }
+
+        let set: LineChartDataSet = LineChartDataSet(values: values, label: nil)
+        set.axisDependency = .left
+        
+        var dataSets: [LineChartDataSet] = [LineChartDataSet]()
+        dataSets.append(set)
+        
+        let data = LineChartData(dataSets: dataSets)
+        chartView.data = data
+        
+        if values.count > Int(maxValuesInViewport) {
+            chartView.setVisibleXRangeMaximum(maxValuesInViewport)
+        }
     }
 
     override func didReceiveMemoryWarning() {
